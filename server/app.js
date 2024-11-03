fe4ba664af0e9b17532957023706d9345e58de70
Sebastian Bastida Marin <122469079+developersbm@users.noreply.github.com>
2024-11-03 12:14:48 -0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,16 +7,6 @@ const { initializeApp } = require('firebase/app')
 const { getFirestore, collection, doc, updateDoc, setDoc, getDoc, getDocs, deleteDoc } = require('firebase/firestore');
 const { getAuth } = require('firebase/auth')
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDPW6O7B_NAZ9XGT3Q87rmVvtVww0rx29c",
-  authDomain: "la-hacks-d7b07.firebaseapp.com",
-  projectId: "la-hacks-d7b07",
-  storageBucket: "la-hacks-d7b07.appspot.com",
-  messagingSenderId: "600374351697",
-  appId: "1:600374351697:web:61953058e4ff05d7282570",
-}
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const db = getFirestore()
